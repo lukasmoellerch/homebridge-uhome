bc1e6dc8512bcb83b963b396bc5097213473a180
Lukas Möller <lmoeller@student.ethz.ch>
2021-01-08 11:37:38 +0100

    Initial commit

---
+++ src/controller.ts
@@ -0,0 +1,257 @@
+/* eslint-disable no-prototype-builtins */
+import * as jayson from "jayson/promise";
+import { CONTROLLER_KEYS, THERMOSTAT_KEYS } from "./const";
+type KeyType = Record<string, { addr: number; value: number | string }>;
+type Request = { addr: number; cb: (v: number) => void };
+class Controller {
+  home: UHome;
+  index: number;
+  keys: typeof CONTROLLER_KEYS;
+  constructor(home: UHome, index: number) {
+    this.home = home;
+    this.index = index;
+    this.keys = {} as typeof CONTROLLER_KEYS;
+    for (const keyName in CONTROLLER_KEYS) {
+      if (CONTROLLER_KEYS.hasOwnProperty(keyName)) {
+        const value = CONTROLLER_KEYS[keyName];
+        this.keys[keyName] = {
+          addr: value.addr + 500 * index,
+          value: undefined,
+        };
+      }
+    }
+  }
+
+  getUpdateRequests() {
+    const result: Request[] = [];
+    for (const keyName in this.keys) {
+      if (this.keys.hasOwnProperty(keyName)) {
+        const value = this.keys[keyName];
+        result.push({
+          addr: value.addr,
+          cb: (newValue) => {
+            value.value = newValue;
+          },
+        });
+      }
+    }
+    return result;
+  }
+
+  async set(obj, value) {
+    await this.home.set(obj, value);
+  }
+}
+export class Thermostat {
+  controller: Controller;
+  controllerIndex: number;
+  index: number;
+  keys: typeof THERMOSTAT_KEYS;
+  constructor(controller: Controller, index: number) {
+    this.controller = controller;
+    this.controllerIndex = controller.index;
+    this.index = index;
+    this.keys = {} as typeof THERMOSTAT_KEYS;
+    for (const keyName in THERMOSTAT_KEYS) {
+      if (THERMOSTAT_KEYS.hasOwnProperty(keyName)) {
+        const value = THERMOSTAT_KEYS[keyName];
+        this.keys[keyName] = {
+          addr: value.addr + 500 * this.controllerIndex + 40 * index,
+          value: undefined,
+        };
+      }
+    }
+  }
+
+  getUpdateRequests() {
+    const result: Request[] = [];
+    for (const keyName in this.keys) {
+      if (this.keys.hasOwnProperty(keyName)) {
+        const value = this.keys[keyName];
+        result.push({
+          addr: value.addr,
+          cb: (newValue) => {
+            value.value = newValue;
+          },
+        });
+      }
+    }
+    return result;
+  }
+
+  get roomName() {
+    return (this.keys.room_name.value as unknown) as string;
+  }
+
+  get temperature() {
+    return this.keys.room_temperature.value;
+  }
+
+  get setpoint() {
+    return this.keys.room_setpoint.value;
+  }
+
+  set setpoint(newValue) {
+    this.keys.room_setpoint.value = newValue;
+  }
+
+  async set(obj, value) {
+    await this.controller.set(obj, value);
+  }
+
+  async setSetpoint(newValue) {
+    await this.set(this.keys.room_setpoint, newValue);
+  }
+
+  get roomInDemand() {
+    return this.keys.room_in_demand.value;
+  }
+}
+export class UHome {
+  host: string;
+  client: jayson.HttpClient;
+  keys: KeyType = {};
+  controllers: Controller[];
+  thermostats: Thermostat[];
+  constructor(host: string) {
+    this.host = host;
+    this.client = jayson.Client.http({
+      host: host,
+      path: "/api",
+      protocol: "http:",
+      method: "POST",
+    });
+
+    this.controllers = [];
+    this.thermostats = [];
+  }
+
+  async init() {
+    await this.initControllers();
+    await this.initThermostats();
+  }
+
+  async initControllers() {
+    this.controllers = [];
+    for (let i = 0; i < 4; i++) {
+      try {
+        const request = await this.client.request("read", {
+          objects: [
+            {
+              id: `${CONTROLLER_KEYS.controller_sv_version.addr + 500 * i}`,
+              properties: { 85: {} },
+            },
+          ],
+        });
+        const value = request.result.objects[0].properties[85].value;
+        if (`${value}` != "0.00") {
+          const controller = new Controller(this, i);
+          this.controllers.push(controller);
+        }
+      } catch (error) {
+        continue;
+      }
+    }
+  }
+
+  async initThermostats() {
+    this.thermostats = [];
+    for (const controller of this.controllers) {
+      for (let index = 0; index < 12; index++) {
+        try {
+          const request = await this.client.request("read", {
+            objects: [
+              {
+                id: `${
+                  THERMOSTAT_KEYS.room_setpoint.addr +
+                  500 * controller.index +
+                  40 * index
+                }`,
+                properties: { 85: {} },
+              },
+            ],
+          });
+          const setpoint = request.result.objects[0].properties[85].value;
+          if (setpoint > 0 && setpoint < 50) {
+            const thermostat = new Thermostat(controller, index);
+            this.thermostats.push(thermostat);
+          }
+        } catch (error) {
+          continue;
+        }
+      }
+    }
+  }
+
+  getUpdateRequests() {
+    const result: Request[] = [];
+    for (const keyName in this.keys) {
+      if (this.keys.hasOwnProperty(keyName)) {
+        const value = this.keys[keyName];
+        result.push({
+          addr: value.addr,
+          cb: (newValue) => {
+            value.value = newValue;
+          },
+        });
+      }
+    }
+    return result;
+  }
+
+  async set(obj, value) {
+    await this.client.request("write", {
+      objects: [
+        {
+          id: obj.addr,
+          properties: {
+            85: {
+              value: `${value}`,
+            },
+          },
+        },
+      ],
+    });
+    obj.value = value;
+  }
+
+  async update() {
+    let requests: Request[] = [];
+    requests = requests.concat(this.getUpdateRequests());
+    for (let i = 0; i < this.controllers.length; i++) {
+      const controller = this.controllers[i];
+      requests = requests.concat(controller.getUpdateRequests());
+    }
+    for (let i = 0; i < this.thermostats.length; i++) {
+      const thermostat = this.thermostats[i];
+      requests = requests.concat(thermostat.getUpdateRequests());
+    }
+    const callbacksById = {};
+    for (let i = 0; i < requests.length; i++) {
+      const callback = requests[i].cb;
+      callbacksById[requests[i].addr] = callback;
+    }
+    const params = {
+      objects: requests.map((request) => {
+        return {
+          id: `${request.addr}`,
+          properties: {
+            85: {},
+          },
+        };
+      }),
+    };
+    const request = await this.client.request("read", params);
+    const result = request.result;
+    for (let i = 0; i < result.objects.length; i++) {
+      try {
+        const object = result.objects[i];
+        const id = object.id;
+        const value = object.properties[85].value;
+        callbacksById[id](value);
+      } catch (error) {
+        continue;
+      }
+    }
+  }
+}
--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -1,6 +1,17 @@
-import { Service, PlatformAccessory, CharacteristicValue, CharacteristicSetCallback, CharacteristicGetCallback } from 'homebridge';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import {
+  Service,
+  PlatformAccessory,
+  CharacteristicValue,
+  CharacteristicSetCallback,
+  CharacteristicGetCallback,
+  CharacteristicChange,
+  Nullable,
+} from "homebridge";
+import { Characteristic } from "hap-nodejs";
+import { Thermostat } from "./controller";
 
-import { ExampleHomebridgePlatform } from './platform';
+import { ExampleHomebridgePlatform } from "./platform";
 
 /**
  * Platform Accessory
@@ -10,142 +21,151 @@ import { ExampleHomebridgePlatform } from './platform';
 export class ExamplePlatformAccessory {
   private service: Service;
 
-  /**
-   * These are just used to create a working example
-   * You should implement your own code to track the state of your accessory
-   */
-  private exampleStates = {
-    On: false,
-    Brightness: 100,
-  };
-
   constructor(
     private readonly platform: ExampleHomebridgePlatform,
     private readonly accessory: PlatformAccessory,
+    private readonly thermostat: Thermostat
   ) {
-
     // set accessory information
-    this.accessory.getService(this.platform.Service.AccessoryInformation)!
-      .setCharacteristic(this.platform.Characteristic.Manufacturer, 'Default-Manufacturer')
-      .setCharacteristic(this.platform.Characteristic.Model, 'Default-Model')
-      .setCharacteristic(this.platform.Characteristic.SerialNumber, 'Default-Serial');
 
-    // get the LightBulb service if it exists, otherwise create a new LightBulb service
-    // you can create multiple services for each accessory
-    this.service = this.accessory.getService(this.platform.Service.Lightbulb) || this.accessory.addService(this.platform.Service.Lightbulb);
+    this.accessory
+      .getService(this.platform.Service.AccessoryInformation)!
+      .setCharacteristic(
+        this.platform.Characteristic.Manufacturer,
+        "Default-Manufacturer"
+      )
+      .setCharacteristic(this.platform.Characteristic.Model, "Default-Model")
+      .setCharacteristic(
+        this.platform.Characteristic.SerialNumber,
+        "Default-Serial"
+      );
 
-    // set the service name, this is what is displayed as the default name on the Home app
-    // in this example we are using the name we stored in the `accessory.context` in the `discoverDevices` method.
-    this.service.setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.exampleDisplayName);
+    this.service =
+      this.accessory.getService(this.platform.Service.Thermostat) ||
+      this.accessory.addService(this.platform.Service.Thermostat);
 
-    // each service must implement at-minimum the "required characteristics" for the given service type
-    // see https://developers.homebridge.io/#/service/Lightbulb
+    this.service.setCharacteristic(
+      this.platform.Characteristic.Name,
+      thermostat.roomName
+    );
 
-    // register handlers for the On/Off Characteristic
-    this.service.getCharacteristic(this.platform.Characteristic.On)
-      .on('set', this.setOn.bind(this))                // SET - bind to the `setOn` method below
-      .on('get', this.getOn.bind(this));               // GET - bind to the `getOn` method below
+    this.service
+      .getCharacteristic(
+        this.platform.Characteristic.CurrentHeatingCoolingState
+      )
+      .on("get", this.handleCurrentHeatingCoolingStateGet.bind(this));
 
-    // register handlers for the Brightness Characteristic
-    this.service.getCharacteristic(this.platform.Characteristic.Brightness)
-      .on('set', this.setBrightness.bind(this));       // SET - bind to the 'setBrightness` method below
+    this.service
+      .getCharacteristic(this.platform.Characteristic.TargetHeatingCoolingState)
+      .on("get", this.handleTargetHeatingCoolingStateGet.bind(this))
+      .on("set", this.handleTargetHeatingCoolingStateSet.bind(this));
 
+    this.service
+      .getCharacteristic(this.platform.Characteristic.CurrentTemperature)
+      .on("get", this.handleCurrentTemperatureGet.bind(this));
 
-    /**
-     * Creating multiple services of the same type.
-     * 
-     * To avoid "Cannot add a Service with the same UUID another Service without also defining a unique 'subtype' property." error,
-     * when creating multiple services of the same type, you need to use the following syntax to specify a name and subtype id:
-     * this.accessory.getService('NAME') || this.accessory.addService(this.platform.Service.Lightbulb, 'NAME', 'USER_DEFINED_SUBTYPE_ID');
-     * 
-     * The USER_DEFINED_SUBTYPE must be unique to the platform accessory (if you platform exposes multiple accessories, each accessory
-     * can use the same sub type id.)
-     */
+    this.service
+      .getCharacteristic(this.platform.Characteristic.TargetTemperature)
+      .on("get", this.handleTargetTemperatureGet.bind(this))
+      .on("set", this.handleTargetTemperatureSet.bind(this));
 
-    // Example: add two "motion sensor" services to the accessory
-    const motionSensorOneService = this.accessory.getService('Motion Sensor One Name') ||
-      this.accessory.addService(this.platform.Service.MotionSensor, 'Motion Sensor One Name', 'YourUniqueIdentifier-1');
-
-    const motionSensorTwoService = this.accessory.getService('Motion Sensor Two Name') ||
-      this.accessory.addService(this.platform.Service.MotionSensor, 'Motion Sensor Two Name', 'YourUniqueIdentifier-2');
-
-    /**
-     * Updating characteristics values asynchronously.
-     * 
-     * Example showing how to update the state of a Characteristic asynchronously instead
-     * of using the `on('get')` handlers.
-     * Here we change update the motion sensor trigger states on and off every 10 seconds
-     * the `updateCharacteristic` method.
-     * 
-     */
-    let motionDetected = false;
-    setInterval(() => {
-      // EXAMPLE - inverse the trigger
-      motionDetected = !motionDetected;
-
-      // push the new value to HomeKit
-      motionSensorOneService.updateCharacteristic(this.platform.Characteristic.MotionDetected, motionDetected);
-      motionSensorTwoService.updateCharacteristic(this.platform.Characteristic.MotionDetected, !motionDetected);
-
-      this.platform.log.debug('Triggering motionSensorOneService:', motionDetected);
-      this.platform.log.debug('Triggering motionSensorTwoService:', !motionDetected);
-    }, 10000);
+    this.service
+      .getCharacteristic(this.platform.Characteristic.TemperatureDisplayUnits)
+      .on("get", this.handleTemperatureDisplayUnitsGet.bind(this))
+      .on("set", this.handleTemperatureDisplayUnitsSet.bind(this));
   }
 
   /**
-   * Handle "SET" requests from HomeKit
-   * These are sent when the user changes the state of an accessory, for example, turning on a Light bulb.
+   * Handle requests to get the current value of the "Current Heating Cooling State" characteristic
    */
-  setOn(value: CharacteristicValue, callback: CharacteristicSetCallback) {
+  handleCurrentHeatingCoolingStateGet(callback) {
+    this.platform.log.debug("Triggered GET CurrentHeatingCoolingState");
+    // set this to a valid value for TargetHeatingCoolingState
+    const currentValue = Characteristic.TargetHeatingCoolingState.HEAT;
 
-    // implement your own code to turn your device on/off
-    this.exampleStates.On = value as boolean;
-
-    this.platform.log.debug('Set Characteristic On ->', value);
-
-    // you must call the callback function
-    callback(null);
+    callback(null, currentValue);
   }
 
   /**
-   * Handle the "GET" requests from HomeKit
-   * These are sent when HomeKit wants to know the current state of the accessory, for example, checking if a Light bulb is on.
-   * 
-   * GET requests should return as fast as possbile. A long delay here will result in
-   * HomeKit being unresponsive and a bad user experience in general.
-   * 
-   * If your device takes time to respond you should update the status of your device
-   * asynchronously instead using the `updateCharacteristic` method instead.
-
-   * @example
-   * this.service.updateCharacteristic(this.platform.Characteristic.On, true)
+   * Handle requests to get the current value of the "Target Heating Cooling State" characteristic
    */
-  getOn(callback: CharacteristicGetCallback) {
+  handleTargetHeatingCoolingStateGet(callback) {
+    this.platform.log.debug(
+      "Triggered GET TargetHeatingCoolingState",
+      this.thermostat.roomInDemand
+    );
 
-    // implement your own code to check if the device is on
-    const isOn = this.exampleStates.On;
+    // set this to a valid value for TargetHeatingCoolingState
+    const currentValue = Characteristic.TargetHeatingCoolingState.HEAT;
 
-    this.platform.log.debug('Get Characteristic On ->', isOn);
-
-    // you must call the callback function
-    // the first argument should be null if there were no errors
-    // the second argument should be the value to return
-    callback(null, isOn);
+    callback(null, currentValue);
   }
 
   /**
-   * Handle "SET" requests from HomeKit
-   * These are sent when the user changes the state of an accessory, for example, changing the Brightness
+   * Handle requests to set the "Target Heating Cooling State" characteristic
    */
-  setBrightness(value: CharacteristicValue, callback: CharacteristicSetCallback) {
-
-    // implement your own code to set the brightness
-    this.exampleStates.Brightness = value as number;
-
-    this.platform.log.debug('Set Characteristic Brightness -> ', value);
-
-    // you must call the callback function
-    callback(null);
+  handleTargetHeatingCoolingStateSet(
+    value: CharacteristicValue,
+    cb: CharacteristicSetCallback,
+    _context?: unknown,
+    _connectionID?: string | undefined
+  ) {
+    this.platform.log.debug("Triggered SET TargetHeatingCoolingState:", value);
+    cb(new Error("no."), Characteristic.TargetHeatingCoolingState.HEAT);
   }
 
+  /**
+   * Handle requests to get the current value of the "Current Temperature" characteristic
+   */
+  handleCurrentTemperatureGet(
+    callback: CharacteristicGetCallback<Nullable<CharacteristicValue>>
+  ) {
+    this.platform.log.debug("Triggered GET CurrentTemperature");
+
+    callback(null, this.thermostat.temperature);
+  }
+
+  /**
+   * Handle requests to get the current value of the "Target Temperature" characteristic
+   */
+  handleTargetTemperatureGet(callback) {
+    this.platform.log.debug("Triggered GET TargetTemperature");
+    callback(null, this.thermostat.setpoint);
+  }
+
+  /**
+   * Handle requests to set the "Target Temperature" characteristic
+   */
+  handleTargetTemperatureSet(
+    value: CharacteristicValue,
+    cb: CharacteristicSetCallback,
+    _context?: unknown,
+    _connectionID?: string | undefined
+  ) {
+    this.platform.log.debug("Triggered SET TargetTemperature:", value);
+    this.thermostat.setSetpoint(value).then(() => {
+      cb(null, value);
+    });
+  }
+
+  /**
+   * Handle requests to get the current value of the "Temperature Display Units" characteristic
+   */
+  handleTemperatureDisplayUnitsGet(
+    callback: CharacteristicGetCallback<Nullable<CharacteristicValue>>
+  ) {
+    this.platform.log.debug("Triggered GET TemperatureDisplayUnits");
+
+    // set this to a valid value for TemperatureDisplayUnits
+    const currentValue = Characteristic.TemperatureDisplayUnits.CELSIUS;
+
+    callback(null, currentValue);
+  }
+
+  /**
+   * Handle requests to set the "Temperature Display Units" characteristic
+   */
+  handleTemperatureDisplayUnitsSet(_change: CharacteristicChange) {
+    return;
+  }
 }
